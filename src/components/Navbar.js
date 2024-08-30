769cab5ddfbf071c9c0ac2eb57f0f755f4e163e9
gauravb948 <gauravb948@gmail.com>
2024-08-30 23:08:40 +0530

    meage

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+export default function Navbar(){
+
+    return <div className="navbar">
+        <div className="logo"></div>
+        <div className="nav-links">
+            <ul>
+                <li>Find Doctors</li>
+                <li>Hospitals</li>
+                <li>Medicines</li>
+                <li>Surgeries</li>
+                <li>Software for provider</li>
+                <li>Facilities</li>
+
+            </ul>
+        </div>
+    </div>
+
+}
